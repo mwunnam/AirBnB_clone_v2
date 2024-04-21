bfe94c0fdcf27d0e906d925e8e0b58f1e5d6a478
mwunnam <mwunnam@gmail.com>
2024-04-21 21:33:32 +0000

    done with task 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+'''Script that print something'''
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def display_1():
+    return "Hello HBNB"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_2():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_3(text):
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+@app.route('/python/', defaults={'text': 'is cool'},
+           strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_4(text):
+    text = text.replace('_', ' ')
+    return f"Python {text}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_5(n):
+    return f"{n} is a number"
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def display_6(n):
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
